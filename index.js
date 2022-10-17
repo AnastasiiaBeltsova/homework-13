2b0b3505f2c1d076eccc500c41548182bf426d1f
AnastasiiaBeltsova <102054895+AnastasiiaBeltsova@users.noreply.github.com>
2022-10-17 22:59:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,38 +9,16 @@ function UserTable({ _content, _form, _addButton, _userInfo, _localStorageKeyNam
     this.onSubmit = function () {
         _form.addEventListener('submit', (event) => {
             event.preventDefault();
-            // if (_form.elements['id'].value) {
-            //     this.updateUser({
-            //         name: _form.elements['name'].value,
-            //         age: _form.elements['age'].value,
-            //         phone: _form.elements['phone'].value,
-            //         id: _form.elements['id'].value,
-            //     })
-            // } else {
-                this.addUser({
+            this.addUser({
                     name: _form.elements['name'].value,
                     age: _form.elements['age'].value,
                     phone: _form.elements['phone'].value,
                     id: Math.floor(Math.random() * 100),
-                })
-            // }
+                });
             _form.reset();
             _form.classList.remove('open');
         })
     }
-
-    // this.updateUser = function (user) {
-    //     const users = JSON.parse(localStorage.getItem(_localStorageKeyName));
-    //     const newUsers = users.map(function (item) {
-    //         if (+item.id === +user.id) {
-    //             return user;
-    //         }
-    //         return item;
-    //     });
-    //     _content.innerHTML = '';
-    //     newUsers.forEach(user => this.userTemplate(user));
-    //     localStorage.setItem(_localStorageKeyName, JSON.stringify(newUsers));
-    // }
 
     this.addUser = function (user) {
         this.userTemplate(user);
@@ -58,7 +36,6 @@ function UserTable({ _content, _form, _addButton, _userInfo, _localStorageKeyNam
                 `<td>${user.age}</td>`+
                 `<td>`+
                     '<button type="button" class="btn btn-primary js--view">View</button>'+
-                    ' <button type="button" class="btn btn-primary js--edit">Edit</button>'+
                     ' <button type="button" class="btn btn-primary js--delete">Delete</button>'+
                 `</td>`+
             `</tr>`
@@ -70,27 +47,13 @@ function UserTable({ _content, _form, _addButton, _userInfo, _localStorageKeyNam
         }
         viewButton.addEventListener('click', handleView);
 
-        // const editButton = _currentTr.querySelector('.js--edit');
-        // const handleEdit = () => {
-        //     _form.reset();
-        //     _form.classList.add('open');
-        //     _form.elements['id'].value = user.id;
-        //     _form.elements['name'].value = user.name;
-        //     _form.elements['phone'].value = user.phone;
-        //     _form.elements['age'].value = user.age;
-        // }
-        // editButton.addEventListener('click', handleEdit);
-
-
         const deleteButton = _currentTr.querySelector('.js--delete');
         deleteButton.addEventListener('click', () => {
             const array = JSON.parse(localStorage.getItem(_localStorageKeyName));
-
             const newArray = array.filter(el => el.id !== +_currentTr.dataset.id);
             _currentTr.remove();
 
             localStorage.setItem(_localStorageKeyName, JSON.stringify(newArray));
-            
         });
     }
 
